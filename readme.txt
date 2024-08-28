36383d1a5aff9903bef4c992c6251e14298f00f0
Gareth Bult <gareth@madpenguin.uk>
2024-08-28 21:51:37 +0000

    Mostly just new versioning

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: madpenguin
 Tags: static, static-site, static-wordpress, static-page, static-site-generation
 Requires at least: 6.5
 Tested up to: 6.6.1
-Stable tag: 1.0.144
+Stable tag: 1.0.165
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- scripts/roll_version.py
+++ scripts/roll_version.py
@@ -17,7 +17,7 @@ def Process(filename, label, newline):
         lines = text.split("\n")
         output = []
         for line in lines:
-            if line.startswith(label):
+            if line.strip().startswith(label):
                 line = newline
             output.append(line)
     with open(filename, 'w') as io:  
@@ -26,8 +26,8 @@ def Process(filename, label, newline):
 v = f'{version["major"]}.{version["minor"]}.{version["sub"]}'
 
 Process('src_php/make-me-static.php', "define( 'MAKE_ME_STATIC_VERSION", f"define( 'MAKE_ME_STATIC_VERSION', '{v}' );")
-Process('src_php/make-me-static.php', ' * Version:', f' * Version:           	{v}')
-Process('src_vue/package.json', '  "version":', f'  "version": "{v}",')
+Process('src_php/make-me-static.php', '* Version:', f' * Version:           	{v}')
+Process('src_vue/package.json', '"version"', f'    "version": "{v}",')
 Process('readme.txt', 'Stable tag: ', f'Stable tag: {v}')
 
 print(f'Updated to: {version["major"]}.{version["minor"]}.{version["sub"]}')
